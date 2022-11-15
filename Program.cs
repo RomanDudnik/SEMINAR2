878d935dc466154dda38c0498093de4b0bfc3ff3
«Roman <research777@mail.ru>
2022-11-15 12:03:17 +0300

    SEMINAR2_2+3

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ int res = Digits(user_num);                          //вызов метода, 
 Console.WriteLine($"The biggest digits is {res} ");
 */
 
-//Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+//9.Напишите программу, которая выводит случайное число из отрезка [10, 99] 
 //и показывает наибольшую цифру числа.
 //78 -> 8
 //12 -> 2
@@ -34,3 +34,46 @@ Console.WriteLine($"The biggest digits is {res} ");
 
 
 
+//Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно a и b.
+/*
+void Cratnost(int num, int a, int b)       //метод для проверки на ратнось, без возвращения -void
+{
+    if(num % a == 0 && num % b == 0)           //проверка на кратнось
+    {
+        Console.WriteLine($"Your number {num} is multirle of {a} and {b}");
+    }
+    else 
+    {
+        Console.WriteLine($"Your number {num} is not multirle of {a} and {b}");
+    }    
+}
+//Запрос данных у пользователя
+Console.Write("Enter your number: ");
+int user_num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter your 1 divider: ");
+int divider1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter your 2 divider: ");
+int divider2 = Convert.ToInt32(Console.ReadLine());
+//Вызов метода
+Cratnost(user_num, divider1, divider2);               //вызываем метод в самой программе
+*/
+
+//Написать программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+//456 -> 46, 789 -> 79
+
+int DeleteDec()                                         //метод
+{
+    int randomNum = new Random().Next(100, 1000);         //введение рандомного трехзначного  числа
+    Console.WriteLine("Your number is: " + randomNum);     // вывод этого числа на экран
+
+    int fut_dec = randomNum / 100;    //деление нацело
+    int fut_ed = randomNum % 10;      //деление с остатком
+
+    int new_num = fut_dec * 10 + fut_ed;                     //нахождение числа без второй цифры
+    return new_num;                                  //возвращение числа без второй цифры
+}
+
+Console.WriteLine($"Your new number is {DeleteDec()} ");         //вывод результата через метод
